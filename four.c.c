786e4e6b94447dc81bef86c4abb616573eaf193d
Janvi Panchal <110367464+Janvipanchal0609@users.noreply.github.com>
2023-02-16 10:01:46 +0530

    Add files via upload

---
+++ four.c.c
@@ -0,0 +1,28 @@
+//Wap to print addition of two numbers
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("Enter two numbers : ");
+	scanf("%d %d" ,&a,&b);
+	
+	printf("Addition =%d",a+b);
+	
+	printf("Enter two numbers : ");
+	scanf("%d %d" ,&a,&b);
+	
+      printf("Substraction =%d",a+b);
+      
+      printf("Enter two numbers : ");
+	scanf("%d %d" ,&a,&b);
+	
+      printf("Multiplication =%d",a+b);
+	
+	return 0;
+	
+}
+
+
