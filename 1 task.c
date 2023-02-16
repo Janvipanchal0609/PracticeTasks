e94731409250f3cd5190fe411c79ca1b7fed256a
Janvi Panchal <110367464+Janvipanchal0609@users.noreply.github.com>
2023-02-16 10:01:30 +0530

    Add files via upload

---
+++ 1 task.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+ int main()
+ {
+ 	printf("\t\t\t\tEnter Details\n\n");
+ 	
+ 	char name[20];
+ 	printf("Enter Name:\n");
+ 	gets(name);
+ 	printf("\nName: %s \n",name);
+ 	
+ 	char age[20];
+ 	printf("\nEnter Age:\n");
+ 	gets(age);
+ 	printf("\nAge: %s \n",age);
+ 	
+ 	char gender[20];
+ 	printf("\nEnter Gender:\n");
+ 	gets(gender);
+ 	printf("\nGender: %s \n",gender);
+ 	
+ 	char email[20];
+ 	printf("\nEnter Email:\n");
+ 	gets(email);
+ 	printf("\nEmail: %s \n",email);
+ 	
+      char contact[20];
+ 	printf("\nEnter Contact-no:\n");
+ 	gets(contact);
+ 	printf("\nContact-no: %s \n",contact);
+ 	
+      char address[20];
+ 	printf("\nEnter Address:\n");
+ 	gets(address);
+ 	printf("\nAddress: %s \n",address);
+ 	
+      char pin[20];
+ 	printf("\nEnter Pincode:\n");
+ 	gets(pin);
+ 	printf("\nPincode: %s \n",pin);
+ 	
+ }
+ 
+ 
