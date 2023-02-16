0d72a84a0a2a89fcf6dfca21d8ff09a36c4de641
Janvi Panchal <110367464+Janvipanchal0609@users.noreply.github.com>
2023-02-16 09:57:58 +0530

    Add files via upload

---
+++ second.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+      printf("task : 2 : \n \n");
+      printf("-----------------------------YOUR DETAILS------------------------------------\n");
+      printf("\t\t\t name:\t\tJanvi\n");
+      printf("\t\t\t Email:\t\tjanvipanchal0609@gmail.com\n");
+      printf("\t\t\t DOB:\t\t2003/09/06\n");
+      printf("\t\t\t Age:\t\t20\n");
+      printf("\t\t\t Gender:\tFemale\n");
+      printf("\t\t\t Hobbies:\tsinging\n");
+      printf("\t\t\t Address:\tA-5 chahat residency r.t.o road vastral ahemdabad\n");
+      
+}
