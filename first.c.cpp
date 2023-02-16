815e47a407c57deea56435924439667b9aad2d47
Janvi Panchal <110367464+Janvipanchal0609@users.noreply.github.com>
2023-02-16 08:31:01 +0530

    Add files via upload

---
+++ first.c.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b;
+	
+	printf("Enter two number : ");
+	scanf("%d %d",&a,&b);
+	
+	
+	printf("\nAddition  = %d\n",a+b);
+	printf("\nSubtraction =%d\n",a-b);
+	printf("\nMultiplication =%d\n",a*b);
+	printf("\nDivision =%d\n",a/b);
+	printf("\nModules =%d\n",a%b);
+	return 0;
+}
